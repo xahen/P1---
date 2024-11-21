c9933da7dd81e3515031fb3af4034b871f706615
xahen <18694387+xahen@users.noreply.github.com>
2024-11-21 16:05:59 +0100

    initialized functions

--- src/resource_calculation.c
+++ src/resource_calculation.c
@@ -2,4 +2,22 @@
 #include <stdlib.h>
 
 #include "resource_calculation.h"
-#include "package_distribution.h"
+
+package_t temp_package() {
+    package_t package;
+    package.height = 0.5; // meters
+    package.width = 0.5; // meters
+    package.length = 0.5; // meters
+    package.weight = 1; // kilo
+    package.priority = 1; // how fast it needs to be delivered
+    package.node_id = 1; // the id of the node it needs to be delivered to
+    package.truck_id = 1; // the id of the truck it's being delivered in
+    package.delivery_status = NOT_DELIVERED; // the status of it's delivery
+
+    return package;
+}
+
+int calculate_trucks(int orders, package_t package) {
+    
+
+}
---
+++ src/resource_calculation.h
@@ -0,0 +1,4 @@
+#include <package_distribution.h>
+
+package_t temp_package();
+int calculate_trucks(int orders, package_t package);
