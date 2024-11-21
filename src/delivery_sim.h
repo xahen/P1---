8d0b90496a760fd4e1490c72571584ebae96d0da
SCHELBECK123 <TG58DO@student.aau.dk>
2024-11-21 16:20:52 +0100

    Print node, package and truck function
    - Added a print function for nodes, packages and trucks.
    
    Co-Authored-By: Oliver Dalgaard <87284361+oliverdalgaard@users.noreply.github.com>

--- src/delivery_sim.h
+++ src/delivery_sim.h
@@ -1,2 +1,6 @@
 #include "package_distribution.h"
 
+void print_package(int selector, package_t package);
+void print_node(int selector, node_t node);
+void print_truck(int selector, truck_t truck);
+
