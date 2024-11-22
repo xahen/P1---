a5f751fea86f32c3f086d636124deb8d1cd003bc
xahen <18694387+xahen@users.noreply.github.com>
2024-11-22 11:09:36 +0100

    changed variable names for clarity

--- src/resource_calculation.c
+++ src/resource_calculation.c
@@ -5,17 +5,17 @@
 #include "delivery_sim.h"
 
 // This can be a different value. Should we not just calculate it in calculate_trucks()?
-#define truck_volume 41.472
+#define TRUCK_VOLUME 41.472
 
 // Maybe we should take weight into account as well.
 void calculate_trucks(package_t package, double *volume_filled, int *truck_amount) {
-    double volume = package.height * package.width * package.length;
+    double package_volume = package.height * package.width * package.length;
 
-    if (*volume_filled + volume < truck_volume) {
-        *volume_filled += volume;
+    if (*volume_filled + package_volume < TRUCK_VOLUME) {
+        *volume_filled += package_volume;
     } else {
         *volume_filled = 0;
-        *volume_filled += volume;
+        *volume_filled += package_volume;
         *truck_amount += 1;
     }
 }
--- src/run_program.c
+++ src/run_program.c
@@ -1,6 +1,3 @@
-#include <delivery_sim.h>
-#include <package_distribution.h>
-#include <resource_calculation.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
