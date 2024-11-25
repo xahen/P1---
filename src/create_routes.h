02e42da0aefb285376e632de474e97632ca6f796
kresten-j <kads24@student.aau.dk>
2024-11-25 21:10:17 +0100

    Moved content to create_routes file

---
+++ src/create_routes.h
@@ -0,0 +1,7 @@
+#include "package_distribution.h"
+
+void add_edge(graph_t *graph, int node_src, int node_dest);
+
+void display_matrix(graph_t *graph);
+
+void free_matrix(graph_t *graph);
--- src/delivery_algorithm.h
+++ src/delivery_algorithm.h
@@ -1,7 +1,2 @@
 #include "package_distribution.h"
 
-void add_edge(graph_t *graph, int node_src, int node_dest);
-
-void display_matrix(graph_t *graph);
-
-void free_matrix(graph_t *graph);
