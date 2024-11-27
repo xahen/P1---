2584395138c46ce4c77b534980926099f363430e
Oliver Dalgaard <oliver@ihd.dk>
2024-11-27 12:54:58 +0100

    Fixed structs and create functions

--- src/package_distribution.c
+++ src/package_distribution.c
@@ -3,41 +3,40 @@
 
 #include "package_distribution.h"
 
-truck_t create_truck(int id) {
-    truck_t truck;
-    truck.id = id;
+truck_t *create_truck(int id) {
+    truck_t *truck = (truck_t*)malloc(sizeof(truck_t));
+    truck->id = id;
 
-    truck.average_speed = 60;
-    truck.max_volume = 19;
-    truck.max_weight = 3500;
+    truck->average_speed = 60;
+    truck->max_volume = 19;
+    truck->max_weight = 3500;
 
     return truck;
 }
 
-package_t create_package(int priority, int node_id, int truck_id, double height, double width, double length, double weight) {
-    package_t package;
-    package.priority = priority;
-    package.node_id = node_id;
-    package.truck_id = truck_id;
-    package.height = height;
-    package.width = width;
-    package.length = length;
-    package.weight = weight;
+package_t *create_package(int priority, int node_id, int truck_id, double height, double width, double length, double weight) {
+    package_t *package = (package_t*)malloc(sizeof(package_t));
+    package->priority = priority;
+    package->node_id = node_id;
+    package->truck_id = truck_id;
+    package->height = height;
+    package->width = width;
+    package->length = length;
+    package->weight = weight;
 
-    package.delivery_status = NOT_DELIVERED;
+    package->delivery_status = NOT_DELIVERED;
 
     return package;
 }
 
-node_t create_node(int location_x, int location_y, int id) {
-    node_t node;
-    node.location_x = location_x;
-    node.location_y = location_y;
-    node.id = id;
+node_t *create_node(int location_x, int location_y, int id) {
+    node_t *node = (node_t*)malloc(sizeof(node_t));
+    node->location_x = location_x;
+    node->location_y = location_y;
+    node->id = id;
 
     for (int i = 0; i < 30; i++) {
-        package_t package = create_package(-1, -1, -1, 0, 0, 0, 0);
-        node.packages[i] = package;
+        node->packages[i] = *create_package(-1, -1, -1, 0, 0, 0, 0);
     }
 
     return node;
