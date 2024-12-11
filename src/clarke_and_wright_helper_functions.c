e12431094838d9d2f732af7f6894ee10a501182e
Oliver Dalgaard <oliver@ihd.dk>
2024-12-11 20:59:11 +0100

    Clarke and wright algorithm

--- src/clarke_and_wright_helper_functions.c
+++ src/clarke_and_wright_helper_functions.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include "clarke_and_wright_helper_functions.h"
+#include "astar_helper_functions.h"
 
 savings_t *savings(node_t *distribution_node, graph_t *optimized_matrix) {
     int amount_of_pairs = (optimized_matrix->nodes - 1) * (optimized_matrix->nodes - 2) / 2;
@@ -49,4 +50,66 @@ void push_savings(savings_t savings, savings_t *list, int size) {
     }
     printf("List is full!\n");
     exit(EXIT_FAILURE);
+}
+
+int is_interior(node_t *node, node_t **list, int size) {
+    int index = get_index(list, node, size);
+
+    // Return false if node is not in route
+    if (index == -1) return 0;
+
+    // Return false if node is first or last in route
+    if (index == 0 || index == size - 1 || list[index + 1] == NULL) return 0;
+
+    return 1;
+}
+
+node_t **merge_routes(node_t **route_one, node_t **route_two, int size, savings_t savings_struct) {
+    node_t **list = (node_t**)calloc(size, sizeof(node_t*));
+
+    int index_i = get_index(route_one, savings_struct.i, size);
+    int index_j = get_index(route_two, savings_struct.j, size);
+
+    if (!(index_i == size - 1 || list[index_i + 1] == NULL)) {
+        reverse_list(route_one, size);
+    }
+
+    if (index_j != 0) {
+        reverse_list(route_two, size);
+    }
+
+    return list;
+}
+
+int get_index(node_t **list, node_t *node, int size) {
+    int index = -1;
+
+    for (int i = 0; i < size; i++) {
+        if (list[i] == node) index = i;
+        if (list[i] == NULL) break;
+    }
+
+    return index;
+}
+
+void reverse_list(node_t **list, int size) {
+    node_t **temp = (node_t**)calloc(size, sizeof(node_t*));
+    int count = 0;
+    for (int i = size-1; i >= 0; i--) {
+        if (list[i] == NULL) continue;
+        temp[count] = list[i];
+        count++;
+    }
+
+    for (int i = 0; i < size; i++) {
+        list[i] = temp[i];
+    }
+    free(temp);
+}
+
+int size_of_list_without_null(node_t **list, int size) {
+    for (int i = 0; i < size; i++) {
+        if (list[i] == NULL) return i;
+    }
+    return size;
 }
--- src/clarke_and_wright_helper_functions.h
+++ src/clarke_and_wright_helper_functions.h
@@ -7,7 +7,11 @@ typedef struct {
 } savings_t;
 
 int savings_compare(const void *a, const void *b);
-
 savings_t *savings(node_t *distribution_node, graph_t *optimized_matrix);
-
 void push_savings(savings_t savings, savings_t *list, int size);
+
+int is_interior(node_t *node, node_t **list, int size);
+node_t **merge_routes(node_t **route_one, node_t **route_two, int size, savings_t savings_struct);
+int get_index(node_t **list, node_t *node, int size);
+void reverse_list(node_t **list, int size);
+int size_of_list_without_null(node_t **list, int size);
