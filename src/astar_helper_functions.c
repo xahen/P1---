5f925c17e975a676540dfbbd6279a2f93aa7f2f5
Oliver Dalgaard <oliver@ihd.dk>
2024-12-18 16:20:50 +0100

    Removed reconstruct_path function

--- src/astar_helper_functions.c
+++ src/astar_helper_functions.c
@@ -4,24 +4,6 @@
 
 #include "astar_helper_functions.h"
 
-int *reconstruct_path(node_t *node, int nodes_amount) {
-    int count = 0;
-    int *list = (int*)calloc(nodes_amount, sizeof(int));
-    list[count] = node->id;
-    reconstruct_path_rec(node->parent, list, &count);
-    return list;
-}
-
-void reconstruct_path_rec(node_t *node_parent, int *list, int *count) {
-    if (node_parent == NULL) {
-        return;
-    }
-
-    *count++;
-    list[*count] = node_parent->id;
-    reconstruct_path_rec(node_parent->parent, list, count);
-}
-
 // Heuristic function - calculates distance as a direct line between two nodes
 double heuristic(node_t current_node, node_t current_node_neighbour) {
     /*double calc_x = current_node.location_x - current_node_neighbour.location_x;
