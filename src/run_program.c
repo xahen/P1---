7c59bf333f6ffc674e9f59d58a9e958dba1e1324
Oliver Dalgaard <87284361+oliverdalgaard@users.noreply.github.com>
2025-01-16 00:05:04 +0100

    Update run_program.c

--- src/run_program.c
+++ src/run_program.c
@@ -9,11 +9,50 @@
 
 int main(void) {
     int start_time = time(NULL);
-    //int seed = time(NULL);
-    int seed = 1734538595;
+    int seed = time(NULL);
+    //int seed = 1734538595;
     srand(seed);
 
-    graph_t *graph = generate_random_graph();
+    int amount_of_nodes = 0;
+    while (amount_of_nodes < 4 || amount_of_nodes > 26) {
+        printf("Input amount of nodes (4-26): \n");
+        scanf("%d", &amount_of_nodes);
+    }
+    int amount_of_trucks = 0;
+    while (amount_of_trucks < 1) {
+        printf("Input amount of trucks (>=1): \n");
+        scanf("%d", &amount_of_trucks);
+    }
+    int amount_of_staff = 0;
+    while (amount_of_staff < amount_of_trucks) {
+        printf("Input amount of staff (>=%d): \n", amount_of_trucks);
+        scanf("%d", &amount_of_staff);
+    }
+    int amount_of_packages = 0;
+    while (amount_of_packages < amount_of_nodes) {
+        printf("Input amount of packages (>=%d): \n", amount_of_nodes);
+        scanf("%d", &amount_of_packages);
+    }
+
+    double volume_filled = 0;
+    double weight_filled = 0;
+    int truck_amount = 0;
+    for (int i = 0; i < amount_of_packages; i++) {
+        package_t *package = generate_random_package();
+        calculate_trucks(*package, &volume_filled, &weight_filled, &truck_amount);
+        if (truck_amount > 1) {
+            printf("The trucks can't hold any more packages!\n");
+            printf("Packages left in the terminal: %d\n", amount_of_packages - i);
+            break;
+        }
+    }
+    printf("Press any key to continue...\n");
+    printf("\n\n");
+    getchar();
+    getchar();
+
+
+    graph_t *graph = generate_random_graph(amount_of_nodes);
     graph_t optimized_matrix = *graph;
 
     a_star_matrix_t a_star_matrix = {
