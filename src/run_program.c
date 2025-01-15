8d299a8cf626658170b13a47196daa9c8d26f1c7
Oliver Dalgaard <87284361+oliverdalgaard@users.noreply.github.com>
2025-01-15 23:50:47 +0100

    Update run_program.c

--- src/run_program.c
+++ src/run_program.c
@@ -1,7 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "delivery_algorithm.h"
+#include "delivery_sim.h"
 #include "resource_calculation.h"
 #include "create_routes.h"
 
 #include <math.h>
+
 int main(void) {
     int start_time = time(NULL);
     int seed = time(NULL);
